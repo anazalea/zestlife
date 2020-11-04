452a4815e2ee8be5ce5405dd4ee9215358fc52c7
Chris <cluciuk@kinaxis.com>
2020-11-04 09:21:35 -0500

    added structure for menus

--- main.py
+++ main.py
@@ -13,9 +13,95 @@ from analog_clock import AnalogClock
 from weather import Stars, Sun, Cloud
 from employee import Employee
 
+#Constants
+#FONT_STYLE = './resources/dpcomic.ttf'
+#FONT_STYLE = './resources/ARCADECLASSIC.ttf'
+FONT_STYLE = './resources/joystix-monospace.ttf'
+
+#Helper functions
+def text_objects(text, font, color):
+    text_surface = font.render(text, True, color)
+    return text_surface, text_surface.get_rect()
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+def draw_rect_alpha(surface, color, rect):
+    shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
+    pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
+    surface.blit(shape_surf, rect)
+
+def button(surface, text, active_color, inactive_color, rect, font, click):
+    mouse = pygame.mouse.get_pos()
+    return_value = False
+    if rect[0] < mouse[0] < rect[0] + rect[2] and rect[1] < mouse[1] < rect[1] + rect[3]:
+        #Mouse is over the button
+        draw_rect_alpha(surface, active_color, rect)
+        #Did the user click
+        if click and pygame.time.get_ticks() > 100:
+            return_value = True
+    else:
+        draw_rect_alpha(surface, inactive_color, rect)
+    text_surf, text_rect = text_objects(text, font, (255, 255, 255))
+    text_rect.center = (int(rect[0] + rect[2] / 2), int(rect[1] + rect[3] / 2))
+    surface.blit(text_surf, text_rect)
+    return return_value
+
+def main_menu():
+    pygame.init()
+    pygame.mouse.set_visible(1)
+    screen = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("LEMONADE")
+    done = False
+    click = False
+    background = create_menu_background(screen)
+    scenery = pygame.image.load('./resources/title.png')
+    #stand = pygame.image.load('./resources/stand.png')
+
+    while not done:
+        screen.blit(background, (0,0))
+        screen.blit(scenery, (0,0))
+        #screen.blit(stand, (100,0))
+        font = pygame.font.Font(FONT_STYLE,25) #Edit fonts here
+        draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
+        buttons = [False]*2
+        button_coords = [(290, 385, 250, 50),(290, 460, 250, 50)]
+        buttons[0] = button(screen, 'Start Game', (0,0,0,100), (0,0,0,255), button_coords[0], font, click)
+        buttons[1] = button(screen, 'Quit', (0,0,0,100), (0,0,0,255), button_coords[1], font, click)
+        if click:
+            if buttons[0]:
+                play()
+            if buttons[1]:
+                done = True
+        click = False
+        pygame.display.update()
+
+        #Events
+        for event in pygame.event.get():
+            # Close button clicked
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                #Clicked on start game
+                if event.button == 1:
+                    click = True
+            if event.type == pygame.KEYDOWN:
+                # Escape key pressed
+                if event.key == pygame.K_ESCAPE:
+                    done = True
 
 def predict_demand(date):
     return 35
+
+def create_menu_background(screen):
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    color = (143, 181, 242)
+    background.fill(color)
+    return background
 
 def create_background(screen, current_time):
     background = pygame.Surface(screen.get_size())
@@ -54,73 +140,29 @@ def play():
 
     # RECIPE STUFF
     recipe = Recipe(lemon_juice=40, sugar=35, water=300, ice=5, straw='no')
-    recipe_theme = pygame_menu.themes.THEME_DEFAULT.copy()
-    recipe_theme.background_color = (0, 0, 0, 100)  # Enable transparency
-    recipe_menu = pygame_menu.Menu(
-        theme=recipe_theme,
-        height=400,
-        width=600,
-        onclose=pygame_menu.events.RESET,
-        title='Recipe',
-    )
-    recipe_menu.add_text_input('Lemon Juice [ml] :', default= recipe.lemon_juice,
-                                     onchange=recipe.validate_lemonjuice, maxwidth = 100,
-                                     maxwidth_dynamically_update = False)
-    recipe_menu.add_text_input('Sugar [g] :', default=recipe.sugar,
-                                     onchange=recipe.validate_sugar)
-    recipe_menu.add_text_input('Water [ml] :', default=recipe.water,
-                                     onchange=recipe.validate_water)
-    recipe_menu.add_text_input('Ice [cubes] :', default=recipe.ice,
-                                     onchange=recipe.validate_ice)
-    recipe_menu.add_selector('Straw Type ',
-                           [('None', 'no'),
-                            ('Plastic', 'plastic'),
-                            ('Paper', 'paper')],
-                           onchange = recipe.validate_straw,
-                           selector_id='select_straw')
-                                     
-
-
-    recipe_menu.add_button('CLOSE', pygame_menu.events.CLOSE)
-    recipe_menu.disable()
+    recipe_menu(recipe)
     recipe_button = pygbutton.PygButton((800-80, 520, 64, 64), normal = './resources/flask.png')
-
-
-
 
     n_customers_today = predict_demand(current_datetime.date())
     customers = [Customer(screen, arrival_time_generator, preference_generator) for i in range(n_customers_today)]
     recent_customer_thought = ''
 
     # PRICE
-    price_theme = pygame_menu.themes.THEME_DEFAULT.copy()
-    price_theme.background_color = (0, 0, 0, 100)  # Enable transparency
-    price_menu = pygame_menu.Menu(
-        theme=price_theme,
-        height=400,
-        width=600,
-        onclose=pygame_menu.events.RESET,
-        title='PRICING',
-    )
-    price_menu.add_text_input('Price/Cup [$] :', default = str(np.round(lemonade_stand.price+0.001,2)),
-                                     onchange = lemonade_stand.validate_price)
-    price_menu.add_button('CLOSE', pygame_menu.events.CLOSE)
-    price_menu.disable()
-    price_button = pygbutton.PygButton((800-80-8-64, 520, 64, 64), 
+    price_menu(lemonade_stand)
+    price_button = pygbutton.PygButton((800-80-8-64, 520, 64, 64),
                                     normal = './resources/dollar-coin.png')
-
 
     fps = 60
     done = False
     pause = False
-    
+
 
     while not done:
         screen.blit(background, (0,0))
         screen.blit(scenery, (0,0))
         time_passed_seconds = clock.tick(fps) / 1000.0
-        
-        font = pygame.font.Font(None,25)
+
+        font = pygame.font.Font(FONT_STYLE,15)
         if not pause:
             current_datetime += datetime.timedelta(minutes = game_speed)
             if np.random.randn() > 0.9:
@@ -193,21 +235,18 @@ def play():
             juice_eff = font.render(f'JUICING EFFICIENCY {str(lemonade_stand.juicing_efficiency)} mL/lemon', 1, (214, 26, 13))
             money = font.render(str(lemonade_stand.account_balance)+' $', 1, (0,0,0))
             thoughts = font.render(recent_customer_thought, 1, (0,0,0))
-            
-            screen.blit(time_stamp,[20,20])   
+
+            screen.blit(time_stamp,[20,20])
             screen.blit(current_price, [20,40])
             screen.blit(juice_eff, [20,60])
             screen.blit(n_lemons, [20,80])
             screen.blit(g_sugar, [20,100])
             screen.blit(money, [20,120])
             screen.blit(thoughts, [10,580])
-            
+
             recipe_button.draw(screen)
             price_button.draw(screen)
-            pygame.display.update() 
-
-        if recipe_menu.is_enabled():
-            recipe_menu.mainloop(screen)
+            pygame.display.update()
 
         for event in pygame.event.get():
 
@@ -217,13 +256,11 @@ def play():
 
             buttonEvents = recipe_button.handleEvent(event)
             if 'click' in buttonEvents:
-                recipe_menu.enable()
-                recipe_menu.mainloop(screen)
+                recipe_menu(recipe)
 
             buttonEvents = price_button.handleEvent(event)
             if 'click' in buttonEvents:
-                price_menu.enable()
-                price_menu.mainloop(screen)
+                price_menu(lemonade_stand)
 
             elif event.type == pygame.KEYDOWN:
                 # Escape key pressed
@@ -232,10 +269,10 @@ def play():
 
                 elif event.key == pygame.K_SPACE:
                     pause = not pause
-                
+
                 elif event.key == pygame.K_1:
                     game_speed = 0.25
-                
+
                 elif event.key == pygame.K_2:
                     game_speed = 1
 
@@ -245,5 +282,145 @@ def play():
                 elif event.key == pygame.K_r:
                     recipe_menu.toggle()
 
+def recipe_menu(recipe):
+    pygame.init()
+    pygame.mouse.set_visible(1)
+    screen = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("LEMONADE")
+    done = False
+    click = False
+    background = create_menu_background(screen)
+    recipe_image = pygame.image.load('./resources/background.png') #Update with recipe image
+    ingredients = {'lemon juice' : recipe.lemon_juice, 'sugar' : recipe.sugar, 'water' : recipe.water, 'ice' : recipe.ice} #Get this from another class
+    ingredient_min, ingredient_max = 0, 500
+    recipe_style = ''
+    while not done:
+        screen.blit(background, (0,0))
+        screen.blit(recipe_image, (0,0))
+
+        font = pygame.font.Font(FONT_STYLE,25) #Edit fonts here
+        draw_text('Set your recipe', font, (255, 255, 255), screen, 20, 20)
+
+        #Try to make this into a loop through different ingredients
+        buttons = [False]*2*len(ingredients)
+        index = 0
+        x_start, y_start = 20, 100
+        button_dimension = 25
+        for item, value in ingredients.items():
+            item_name_x = x_start
+            item_name_y = y_start + index*30 #Next line is 30 down
+            button_coords = [(430,y_start + index*30, button_dimension, button_dimension), (480,y_start + index*30, button_dimension, button_dimension), (530,y_start + index*30, button_dimension, button_dimension)]
+            draw_text(item, font, (255, 255, 255), screen , item_name_x, item_name_y)
+            buttons[index*2] = button(screen, '-', (0,0,0,100), (0,0,0,255), button_coords[0], font, click)
+            button(screen, str(value), (0,0,0,0), (0,0,0,0), button_coords[1], font, click)
+            buttons[index*2+1] = button(screen, '+', (0,0,0,100), (0,0,0,255), button_coords[2], font, click)
+            index +=1
+        #Buttons to accept recipe and return to game
+        accept_recipe = button(screen, 'Accept Recipe', (0,0,0,100), (0,0,0,255), (50,500,300,50), font, click)
+        return_to_game = button(screen, 'Resume Game', (0,0,0,100), (0,0,0,255), (400,500,300,50), font, click)
+
+        if click:
+            for i in range(len(buttons)):
+                if buttons[i]:
+                    if i % 2 != 0:
+                        if ingredients[list(ingredients.keys())[int(i/2)]] == ingredient_max:
+                            pass
+                        else:
+                            ingredients[list(ingredients.keys())[int(i/2)]] +=1
+                    else:
+                        if ingredients[list(ingredients.keys())[int((i+1)/2)]] == ingredient_min:
+                            pass
+                        else:
+                            ingredients[list(ingredients.keys())[int((i+1)/2)]] -=1
+            if accept_recipe:
+                recipe.update_ratios(ingredients['lemon juice'], ingredients['sugar'], ingredients['water'], ingredients['ice'])
+                #Write something about the type of lemonade you're making
+                recipe_style = 'The recipe sounds tasty'
+            if return_to_game:
+                done = True
+
+        click = False
+        recipe_message = font.render(recipe_style, 1, (0,0,0))
+        screen.blit(recipe_message, [10,380])
+        pygame.display.update()
+
+        #Events
+        for event in pygame.event.get():
+            # Close button clicked
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                #Clicked on start game
+                if event.button == 1:
+                    click = True
+            if event.type == pygame.KEYDOWN:
+                # Escape key pressed
+                if event.key == pygame.K_ESCAPE:
+                    done = True
+
+def price_menu(lemonade_stand):
+    pygame.init()
+    pygame.mouse.set_visible(1)
+    screen = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("LEMONADE")
+    done = False
+    click = False
+    background = create_menu_background(screen)
+    price_image = pygame.image.load('./resources/background.png') #Update with price image
+    price = lemonade_stand.price
+    price_min, price_max = 0, 50
+    price_style = ''
+    while not done:
+        screen.blit(background, (0,0))
+        screen.blit(price_image, (0,0))
+
+        font = pygame.font.Font(FONT_STYLE,25) #Edit fonts here
+        draw_text('Set your price', font, (255, 255, 255), screen, 20, 20)
+
+        x_start, y_start = 20, 100
+        button_dimension = 25
+        item_name_x = x_start
+        item_name_y = y_start
+        button_coords = [(430,y_start, button_dimension, button_dimension), (490,y_start, button_dimension, button_dimension), (550,y_start, button_dimension, button_dimension)]
+        draw_text('Price', font, (255, 255, 255), screen , item_name_x, item_name_y)
+        button_1 = button(screen, '-', (0,0,0,100), (0,0,0,255), button_coords[0], font, click)
+        button(screen, str(price), (0,0,0,0), (0,0,0,0), button_coords[1], font, click)
+        button_2 = button(screen, '+', (0,0,0,100), (0,0,0,255), button_coords[2], font, click)
+        #Buttons to accept recipe and return to game
+        accept_price = button(screen, 'Accept Price', (0,0,0,100), (0,0,0,255), (50,500,300,50), font, click)
+        return_to_game = button(screen, 'Resume Game', (0,0,0,100), (0,0,0,255), (400,500,300,50), font, click)
+
+        if click:
+            if button_1:
+                price = round(price - 0.01, 2)
+            if button_2:
+                price = round(price + 0.01, 2)
+            if accept_price:
+                lemonade_stand.price = price
+                #Write something about the type of lemonade you're making
+                price_style = 'The price seems reasonable'
+            if return_to_game:
+                done = True
+
+        click = False
+        price_message = font.render(price_style, 1, (0,0,0))
+        screen.blit(price_message, [10,380])
+        pygame.display.update()
+
+        #Events
+        for event in pygame.event.get():
+            # Close button clicked
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                #Clicked on start game
+                if event.button == 1:
+                    click = True
+            if event.type == pygame.KEYDOWN:
+                # Escape key pressed
+                if event.key == pygame.K_ESCAPE:
+                    done = True
+
 if __name__ == '__main__':
-  play()
+    main_menu()
+    #recipe_menu()
