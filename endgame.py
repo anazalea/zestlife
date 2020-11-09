96abd2628e69e3cb378cfb843f1e2fae11e6966d
Anneya Golob <agolob@kinaxis.com>
2020-11-09 17:45:15 -0500

    Make goal reasonable

--- endgame.py
+++ endgame.py
@@ -2,7 +2,7 @@ import pygame
 import menus
 
 def check_victory_condition(lemonade_game):
-    if lemonade_game.lemonade_stand.n_customers_served > 10:
+    if lemonade_game.lemonade_stand.n_customers_served > 100:
         menus.victory_menu(lemonade_game)
         return True
     else:
